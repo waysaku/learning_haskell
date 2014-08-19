d9e4002360f2be23b2a3ad6de241e92dd8097de0
waysaku <waysaku@gmail.com>
2014-08-19 10:01:16 +0900

    learning chapter5

--- chapter5.hs
+++ chapter5.hs
@@ -25,5 +25,11 @@ largestDivisible :: Integer
 largestDivisible = head(filter p [100000, 99999..])
   where p x = x `mod` 3829 == 0
 
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+  | even n = n : chain(n `div` 2)
+  | odd n = n : chain(n * 3 + 1)
 
-
+numLongChains :: Int
+numLongChains = length (filter(\xs -> length xs > 15) (map chain [1..100]))
