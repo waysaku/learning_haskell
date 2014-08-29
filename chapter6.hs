03181579fe509dae9b672c75675fffc9f5698c47
waysaku <waysaku@gmail.com>
2014-08-29 10:00:02 +0900

    learning chapter6

--- chapter6.hs
+++ chapter6.hs
@@ -1,10 +1,15 @@
 import Data.List
+import Data.Char
 import qualified Data.Map as M
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
 
-
-
 wordNums :: String -> [(String, Int)]
 wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+searcharr :: (Ord a) =>  [a] -> [a] -> Bool
+searcharr xs ys = length [x | x <- tails ys, x == xs] > 0
+
+encode' :: Int -> String -> String
+encode' n xs = map (\z -> chr z) $ map (\y -> y - n) $ map (\x -> ord x) xs
