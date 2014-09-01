e937cba369391860a7b0f0aacb672f722e8d3fb4
waysaku <waysaku@gmail.com>
2014-09-01 09:54:08 +0900

    learning chapter6

--- chapter6.hs
+++ chapter6.hs
@@ -32,5 +32,28 @@ phoneBook = [
   ("penny", "853-2492")
   ]
 
-findKey :: (Eq k) => k -> [(k, v)] -> v
-findKey key xs = snd . head . filter (\(k, v) -> key == k) $ xs
+-- findKey :: (Eq k) => k -> [(k, v)] -> v
+-- findKey key xs = snd . head . filter (\(k, v) -> key == k) $ xs
+
+
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+-- findKey key [] = Nothing
+-- findKey key ((k, v): xs)
+--   | key == k = Just v
+--   | otherwise = findKey key xs
+
+findKey key xs = foldr 
+  (\(k, v) acc -> if key == k then Just v else acc) 
+  Nothing xs
+
+phoneBook' :: M.Map String String
+phoneBook' = M.fromList $ [
+  ("betty", "555-2938"), 
+  ("bonnie", "452-2928"), 
+  ("patsy", "493-2928"), 
+  ("lucille", "205-2928"), 
+  ("wendy", "939-8282"), 
+  ("penny", "853-2492")
+  ]
+
