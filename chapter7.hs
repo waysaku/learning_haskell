2b6a0335d682a46977c55fae12e436a5754aa0f7
waysaku <waysaku@gmail.com>
2014-09-05 09:42:10 +0900

    learning chapter7

--- chapter7.hs
+++ chapter7.hs
@@ -32,5 +32,22 @@ data Person = Person {firstName   :: String,
                       flavor      :: String} deriving (Show)
 
 
+{-
+data Car = Car { company :: String,
+                  model :: String,
+                  year :: Int} deriving (Show)
 
+tellCar :: Car -> String
+tellCar (Car { company = c, model = m, year = y}) = 
+  "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+-}
+
+
+data Car a b c = Car {company :: a,
+                      model :: b,
+                      year :: c} deriving(Show)
+
+tellCar' :: (Show a) => Car String String a -> String
+tellCar' (Car {company = c, model = m, year = y}) =
+  "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
 
