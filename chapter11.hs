d85734c61fcac6edf2b0e56ec1a5a6451e0a7ec0
waysaku <waysaku@gmail.com>
2014-09-12 00:12:53 +0900

    learning chapter11

---
+++ chapter11.hs
@@ -0,0 +1,49 @@
+import Data.Char
+import Data.List
+
+{-
+main = do 
+  line <- getLine
+  let line' = reverse line
+  putStrLn $ "You said " ++ line' ++ " backwards!"
+  putStrLn $ "Yes, you said " ++ line' ++ " backwards!"
+-}
+
+{-
+main = do
+  line <- fmap reverse getLine
+  putStrLn $ "You said " ++ line ++ " backwards!"
+  putStrLn $ "Yes, you really said " ++ line ++ " backwards!"
+-}
+
+main = do
+  line <- fmap (intersperse '-' . reverse . map toUpper) getLine
+  putStrLn line
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
