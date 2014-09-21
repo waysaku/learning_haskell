61965f345d1d0ffd237206cfd6368653c544669d
waysaku <waysaku@gmail.com>
2014-09-21 09:44:41 +0900

    learning chapter11

--- chapter11.hs
+++ chapter11.hs
@@ -21,9 +21,11 @@ main = do
   putStrLn line
 
 
+data CMaybe a = CNothing | CJust Int a deriving (Show)
 
-
-
+instance Functor CMaybe where
+  fmap f CNothing = CNothing
+  fmap f (CJust counter x) = CJust (counter + 1) (f x)
 
 
 
